e2ff1b3466e1812979ef2d5ca7294308c3f401bb
Sathvika-0817 <sathvika676@gmail.com>
2025-12-03 15:15:20 +0530

    First commit

---
+++ src/test/java/jenkins/JenkinsPracticeTest.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsPracticeTest {
+	@Test
+	public void jenkinsPracticeTest() {
+		Reporter.log("Hii jenkinsc", true);
+	}
+}
